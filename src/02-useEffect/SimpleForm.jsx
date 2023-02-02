10b22e959fbac4f1109b9660bd0894327294785b
Jeral Pineda <jeral.cris@gmail.com>
2023-02-01 20:03:00 -0600

    Formulario para prueba de useEffect

---
+++ src/02-useEffect/SimpleForm.jsx
@@ -0,0 +1,48 @@
+import { useState } from 'react';
+import { Message } from './Message';
+
+export const SimpleForm = () => {
+  const [formState, setFormState] = useState({
+    username: 'jeral2',
+    email: 'jeral@gmail.com',
+  });
+
+  const { username, email } = formState;
+
+  const onInputChange = ({ target }) => {
+    const { name, value } = target;
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
+
+  return (
+    <>
+      <h1>Formulario Simple</h1>
+      <hr />
+
+      <input
+        //
+        type="text"
+        className="form-control"
+        placeholder="Username"
+        name="username"
+        value={username}
+        onChange={onInputChange}
+      />
+
+      <input
+        //
+        type="email"
+        className="form-control mt-2"
+        placeholder="jeral@gmail.com"
+        name="email"
+        value={email}
+        onChange={onInputChange}
+      />
+
+      {username === 'jeral' && <Message />}
+    </>
+  );
+};
