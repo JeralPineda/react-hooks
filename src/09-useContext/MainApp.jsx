fc58105dba04633584de523aa0093f3d2626515a
Jeral Pineda <jpineda@banhcafe.hn>
2023-03-02 13:57:32 -0600

    Configurando rutas de la app

--- src/09-useContext/MainApp.jsx
+++ src/09-useContext/MainApp.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { HomePage, LoginPage, AboutPage } from './';
 
 export const MainApp = () => {
   return (
     <div>
       <h1>MainApp</h1>
       <hr />
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="about" element={<AboutPage />} />
+        <Route path="login" element={<LoginPage />} />
+
+        <Route path="/*" element={<Navigate to="/about" />} />
+      </Routes>
     </div>
   );
 };
