d2254b09d44f57bc715b9d34e30c80a1fcc57039
Jeral Pineda <jpineda@banhcafe.hn>
2023-03-02 12:21:21 -0600

    Agregando nuevos metodos para saber cuantos todos hay y los marcados

--- src/08-useReducer/TodoApp.jsx
+++ src/08-useReducer/TodoApp.jsx
@@ -1,14 +1,14 @@
-import { useTodo } from '../hooks/useTodo';
+import { useTodo } from '../hooks';
 import { TodoAdd } from './TodoAdd';
 import { TodoList } from './TodoList';
 
 export const TodoApp = () => {
-  const { todos, handleNewTodo, handleRemoveTodo, handleToggleTodo } = useTodo();
+  const { todos, todosCount, pendingTodosCount, handleNewTodo, handleRemoveTodo, handleToggleTodo } = useTodo();
 
   return (
     <div>
       <h1>
-        TodoApp: 10, <small>pendientes: 2</small>
+        TodoApp: {todosCount}, <small>pendientes: {pendingTodosCount}</small>
       </h1>
       <hr />
 
