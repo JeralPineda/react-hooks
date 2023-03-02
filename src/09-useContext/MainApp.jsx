aad4855ab2fc733b37b740d3bd8c47be5dbc5dff
Jeral Pineda <jpineda@banhcafe.hn>
2023-03-02 14:31:21 -0600

    Creando NavLink para la app

--- src/09-useContext/MainApp.jsx
+++ src/09-useContext/MainApp.jsx
@@ -1,17 +1,18 @@
-import React from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
 import { HomePage, LoginPage, AboutPage } from './';
+import { Navbar } from './Navbar';
 
 export const MainApp = () => {
   return (
     <div>
       <h1>MainApp</h1>
+      <Navbar />
       <hr />
 
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="about" element={<AboutPage />} />
-        <Route path="login" element={<LoginPage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/login" element={<LoginPage />} />
 
         <Route path="/*" element={<Navigate to="/about" />} />
       </Routes>
